226bb5e3be8a31f7682bb02d81ee1b70f2287df9
Dillion Verma <dillionverma@hotmail.com>
2018-02-21 00:14:20 -0800

    add antd ui framework

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from 'antd/lib/button';
 class App extends Component {
   render() {
     return (
@@ -13,6 +14,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <Button type="primary">Button</Button>
       </div>
     );
   }
