e2bc81f272997c04e1940b9649dc9e299f4653a1
Dillion Verma <dillion.verma@limebike.com>
2018-03-05 15:13:24 -0800

    separate orders views

---
+++ src/features/orders/OrdersView.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import {
+  Table,
+  Divider,
+  Button,
+  message,
+  Popconfirm,
+  Badge,
+} from 'antd';
+import moment from 'moment';
+
+function handleDeliverClick(e) {
+  message.success('Successfully Delivered');
+  console.log('click left button', e);
+}
+
+function handleRefundClick(e) {
+  //message.info('Click on Refund button.');
+  console.log('click left button', e);
+}
+
+function handleMenuClick(e) {
+  //message.info('Click on menu item.');
+  console.log('click', e);
+}
+
+function refund() {
+  message.success('Successfully Refunded')
+}
+
+function statusComp(text, status) {
+  return (
+    <div>
+      <Badge status={status}/>
+      <span style={{marginRight: '4px'}}>{text}</span>
+    </div>
+  )
+}
+
+function status(text) {
+  switch(text) {
+    case 'ordered':
+      return statusComp(text, 'processing')
+      break;
+    case 'paid':
+      return statusComp(text, 'success')
+      break;
+    case 'cancelled':
+      return statusComp(text, 'error')
+      break;
+  }
+}
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    sorter: true,
+    //width: '30%',
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    width: '12%',
+    render: data => status(data)
+  },
+  {
+    title: 'Table',
+    dataIndex: 'table.number',
+    width: '12%',
+  },
+  {
+    title: 'Price',
+    dataIndex: 'price',
+    width: '12%',
+    render: cents => `$${cents/100}`
+  },
+  {
+    title: 'Ordered',
+    dataIndex: 'created_at',
+    width: '12%',
+    render: date => moment(date).fromNow()
+  },
+  {
+    title: 'Action',
+    dataIndex: '',
+    key: 'x',
+    width: '20%',
+    render: (text, record) => (
+      <div>
+        <Button onClick={handleDeliverClick}>
+          Deliver
+        </Button>
+        <Divider type="vertical"/>
+        <Popconfirm placement="top" title={'Are you sure you want to refund?'} onConfirm={refund} okText="Yes" cancelText="No">
+          <Button onClick={handleRefundClick}>
+            Refund
+          </Button>
+        </Popconfirm>
+      </div>
+    ),
+  }
+];
+
+class OrdersView extends Component {
+
+  render() {
+    return(
+        <Table
+          size="middle"
+          columns={columns}
+          dataSource={this.props.orders}
+          rowKey={order => order.id}
+          pagination={this.props.pagination}
+          loading={this.props.loading}
+          onChange={this.props.handleTableChange}
+        />
+    )
+  }
+}
+export default OrdersView;
