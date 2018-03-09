89d84a8d9ab7cacbcd8ada5b1e49ea7b49c5923b
Dillion Verma <dillionverma@hotmail.com>
2018-03-08 17:42:38 -0800

    fix loggedin state loading

--- src/features/login/LoggedIn.js
+++ src/features/login/LoggedIn.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { Redirect } from 'react-router'
 import { authenticate } from './LoginActions';
+import { Spin, Icon } from 'antd';
 
 class LoggedIn extends Component {
   componentWillMount() {
@@ -11,13 +12,26 @@ class LoggedIn extends Component {
     }
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.status === false) {
+      this.props.history.push('/')
+    }
+  }
+
   render() {
+    const antIcon = <Icon type="loading" style={{ fontSize: 64 }} />;
+    const containerStyles = {
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
     if (this.props.status) {
       return React.cloneElement(React.Children.only(this.props.children), {
         location: this.props.history.location,
       });
     } else {
-      return <Redirect to="/"/>
+      return <div style={containerStyles}><Spin indicator={antIcon}/></div>
     }
   }
 }
