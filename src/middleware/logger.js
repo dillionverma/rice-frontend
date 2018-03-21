088c1ac1d04688bdbc3e62da3486a5949386c672
Dillion Verma <dillionverma@hotmail.com>
2018-03-20 23:58:04 -0700

    remove immutable

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,18 +1,5 @@
 import { createLogger } from 'redux-logger';
-import Immutable from 'immutable';
 
-const logger = createLogger({
-  stateTransformer: (state) => {
-    let newState = {};
-    for (var i of Object.keys(state)) {
-      if (Immutable.Iterable.isIterable(state[i])) {
-        newState[i] = state[i].toJS();
-      } else {
-        newState[i] = state[i];
-      }
-    };
-    return newState;
-  }
-});
+const logger = createLogger();
 
 export default logger;
