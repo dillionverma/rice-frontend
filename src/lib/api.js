08eda6679002554434f9703c46990b26d1a9ed7f
Dillion Verma <dillionverma@hotmail.com>
2018-03-22 20:42:07 -0700

    create demo

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://ricepay.herokuapp.com/api';
 
 var HEADERS = {
   'Content-Type': 'application/json',
