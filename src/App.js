507d68df5f9bed213b883e25176c7e218d46b94f
Dillion Verma <dillionverma@hotmail.com>
2018-04-14 16:54:25 -0700

    setup stripe callback button (#7)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import AppFrame         from './features/appFrame/AppFrame';
-import Landing          from './features/landing/Landing';
+import AppFrame         from './components/appFrame/AppFrame';
+import Landing          from './components/landing/Landing';
 import Dashboard        from './features/dashboard/Dashboard';
 import Tables           from './features/restaurant/Tables';
 import LoginForm        from './features/session/LoginForm';
@@ -10,6 +10,7 @@ import LoggedIn         from './features/session/LoggedIn';
 import Orders           from './features/orders/Orders';
 import Order            from './features/orders/Order';
 import Menus            from './features/menus/Menus';
+import Settings         from './features/settings/Settings';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
@@ -26,6 +27,7 @@ class App extends Component {
                 <Route path='/dashboard'         component={Dashboard} />
                 <Route path='/orders/:id'        component={Order} />
                 <Route path='/orders'            component={Orders} />
+                <Route path='/settings'          component={Settings} />
                 <Route path='/restaurant/tables' component={Tables} />
                 <Route path='/restaurant/menus'  component={Menus} />
               </Switch>
